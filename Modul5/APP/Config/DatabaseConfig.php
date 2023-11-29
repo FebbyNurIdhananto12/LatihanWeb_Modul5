73ee38d09def2b32e1894a2a82330646b2330e01
FebbyNurIdhananto12 <129040396+FebbyNurIdhananto12@users.noreply.github.com>
2023-11-29 20:53:20 +0700

    Add files via upload

---
+++ Modul5/APP/Config/DatabaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Config;
+
+class DatabaseConfig
+{
+    // SETTING KONFIGURASI DATABASE KALIAN
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "modul5";
+    public $port = 3306;
+}
